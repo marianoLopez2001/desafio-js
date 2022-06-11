fd499a29fe952c4fd32a2e02ad0fc265fa2886c3
Mariano Lopez <lopezmariano3108@gmail.com>
2022-06-11 08:16:02 -0300

    elimine comentarios

--- main.js
+++ main.js
@@ -18,37 +18,3 @@ do {
             break;
     }
 } while (preguntarDeVuelta);
-
-// let costoTotal = 0;
-// let precio = 0;
-// let seguirComprando = false;
-// let cantidad = 0;
-// let productos = "";
-
-// do {
-//     productos = prompt(`leche, harina y huevos`);
-//     cantidad = prompt("elija cantidad");
-
-//     switch (productos) {
-//         case "leche":
-//             alert("eligio leche")
-//             precio = 150;
-//             break;
-//         case "harina":
-//             alert("eligio harina")
-//             precio = 100;
-//             break;
-//         case "huevos":
-//             alert("eligio huevos")
-//             precio = 200;
-//             break;
-//         default:
-//             alert("error")
-//             cantidad = 0;
-//             precio = 0;
-//             break;
-//     }
-//     seguirComprando = confirm("desea seguir comprando?")
-//     costoTotal += precio * cantidad;
-// } while (seguirComprando);
-// alert(`el valor total es ${costoTotal}`);
