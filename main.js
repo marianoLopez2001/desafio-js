4ccaa91c74e23c2abde9a9d4203ddc9aaa505746
Mariano Lopez <lopezmariano3108@gmail.com>
2022-06-16 05:39:41 -0300

    hice el desafio

--- main.js
+++ main.js
@@ -1,26 +1,149 @@
-let preguntarDeVuelta = false;
+// const sumaProductos = function (a, b) {
+//     return a + b;
+// }
+
+const multiplicacionCantidad = function (a, b) {
+    return a * b;
+}
+
+const divisionCuotas = function (a, b) {
+    return a / b;
+}
+
+const restarDescuento = function (a, b) {
+    return a - b;
+}
+let repetirBucle = false;
+let precioProductos;
+let cantidadProductos;
+let cuotas;
+
 do {
-    let pregunta = prompt("¿De què color es el semaforo?").toLowerCase();
-    switch (pregunta) {
-        case "verde":
-            alert("Puede pasar");
-            preguntarDeVuelta=false;
+    precioProductosPrompt = prompt(`Elija el producto deseado
+    *Harina
+    *Leche
+    *Huevos`).toLowerCase();
+    switch (precioProductosPrompt) {
+        case "harina":
+            alert("Eligió harina");
+            precioProductos = 200;
+            repetirBucle = false;
             break;
-        case "amarillo":
-            alert("Casi puede pasar");
-            preguntarDeVuelta = true;
+        case "leche":
+            alert("Eligió leche");
+            precioProductos = 150;
+            repetirBucle = false;
             break;
-        case "rojo":
-            alert("no puede pasar");
-            preguntarDeVuelta=true;
+        case "huevos":
+            alert("Eligió huevos");
+            precioProductos = 300;
+            repetirBucle = false;
             break;
         case "esc":
-            alert("eligio salir");
-            preguntarDeVuelta=false;
+            alert("Ha salido");
+            repetirBucle = false;
             break;
         default:
-            alert("Ha ingresado un valor no valido");
-            preguntarDeVuelta = true;
+            alert("error, ingrese un valor valido");
+            repetirBucle = true;
             break;
     }
-} while (preguntarDeVuelta);
+} while (repetirBucle);
+
+do {
+    cantidadProductos = prompt(`Elija la cantidad de productos
+    *1 *2 *3 *4`) 
+    switch (cantidadProductos) {
+        case "1":
+            alert(`eligió ${cantidadProductos} de su producto`)
+            cantidadProductos = 1;
+            repetirBucle = false;
+            break;
+        case "2":
+            alert(`eligió ${cantidadProductos} de su producto`)
+            cantidadProductos = 2;
+            repetirBucle = false;
+            break;
+        case "3":
+            alert(`eligió ${cantidadProductos} de su producto`)
+            cantidadProductos = 3;
+            repetirBucle = false;
+            break;
+        case "4":
+            alert(`eligió ${cantidadProductos} de su producto`)
+            cantidadProductos = 4;
+            repetirBucle = false;
+            break;
+
+        case "esc":
+            alert("ha salido");
+            repetirBucle = true;
+            break;
+
+        default:
+            alert("error, ingrese un valor valido");
+            repetirBucle = true;
+            break;
+    }
+
+} while (repetirBucle);
+
+do {
+    cuotas = prompt(`Especifique el número de cuotas
+    *1 *3 *6 *12`)
+    switch (cuotas) {
+        case "1":
+            cuotas = 1;
+            alert(`Elegiste pagar en ${cuotas} cuotas`);
+            break;
+        case "3":
+            cuotas = 3;
+            alert(`Elegiste pagar en ${cuotas} cuotas`);
+            break;
+        case "6":
+            cuotas = 6;
+            alert(`Elegiste pagar en ${cuotas} cuotas`);
+            break;
+        case "12":
+            cuotas = 12;
+            alert(`Elegiste pagar en ${cuotas} cuotas`);
+            break;
+
+        case "esc":
+            alert("ha salido");
+            repetirBucle = true;
+            break;
+
+        default:
+            alert("error, ingrese un valor valido");
+            repetirBucle = true;
+            break;
+    }
+} while (repetirBucle);
+
+let descuento = confirm("¿Desea un descuento del 15%?")
+let precioProductoYcantidad = multiplicacionCantidad (precioProductos, cantidadProductos);
+
+if (descuento = true) {
+    descuento = precioProductoYcantidad * 15 / 100;
+} else {
+    descuento = 0;
+}
+
+
+function precioFinal () {
+    let precioFinal = restarDescuento (multiplicacionCantidad(precioProductos, cantidadProductos), descuento) 
+    alert(`Su precio final es de ${precioFinal}$.`)};
+
+function precioMensual() {
+    let precioMensual = divisionCuotas (restarDescuento (multiplicacionCantidad(precioProductos, cantidadProductos), descuento), cuotas);
+    alert(`Su precio mensual es ${precioMensual}$`);
+};
+
+precioFinal();
+precioMensual();
+//cuantas cuotas, tiene codigo de descuento, invocar funcion
+
+
+
+
